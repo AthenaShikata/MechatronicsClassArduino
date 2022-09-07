d3d306c282bad732f21dde49cf4b1059f96f1c79
Athena Shikata <evanperes7@gmail.com>
2022-09-07 07:56:11 -0700

    9/6/22

---
+++ 2022-09-06_Pull_Up_Test.ino
@@ -0,0 +1,23 @@
+const int badled = 3;
+const int led = 2;
+const int button = 6;
+int state;
+
+void setup() {
+  pinMode (2, OUTPUT);
+  pinMode (3, OUTPUT);
+  pinMode (6, INPUT);
+  Serial.begin (9600);
+}
+
+void loop() {
+  state = digitalRead (button);
+  Serial.println (state);
+  if (state == 1) {
+    digitalWrite (led, LOW);
+    digitalWrite (badled, HIGH);
+  } else {
+    digitalWrite (led, HIGH);
+    digitalWrite (badled, LOW);
+  }
+}
