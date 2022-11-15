868d39b2b819be8b53e7fa83940aa5702f0b153b
Athena Shikata <99115384+AthenaShikata@users.noreply.github.com>
2022-11-15 09:36:45 -0800

    Update 2022-11-10 Button Timer With millis().ino

--- 2022-11-10 Button Timer With millis().ino
+++ 2022-11-10 Button Timer With millis().ino
@@ -6,35 +6,23 @@ int toggle = 0;
 int button = 0;
 int press = 1;
 
-const int ledToggle = 2;
-const int ledButton = 3;
-
 void setup() {
   Serial.begin (9600);
   pinMode (13, INPUT_PULLUP);
-  pinMode (ledToggle, OUTPUT);
-  pinMode (ledButton, OUTPUT);
 }
 
 void loop() {
   press = digitalRead(13);
-  if (press == 1) {
-    //button up
+  if (press == 1) {                   //press == 1    button up
     button = 0;
-  } else if (press == 0) {
-    //button down
-    if (button == 1) {
-      //button is still down
-    } else if (button == 0) {
-      //button just went down
+  } else if (press == 0) {            //press == 0    button down
+    if (button == 0) {                //button == 0   button just went down
       button = 1;
-      if (toggle == 0) {
-        //timer start
+      if (toggle == 0) {              //toggle == 0   timer start
         toggle = 1;
         start = millis();
         Serial.println ("Timer has started");
-      } else if (toggle == 1) {
-        //timer end
+      } else if (toggle == 1) {       //toggle == 1   timer end
         toggle = 0;
         end = millis();
         duration = end - start;
@@ -43,7 +31,6 @@ void loop() {
         Serial.println (" milliseconds");
       }
     }
+    delay (10);
   }
-  digitalWrite(ledToggle, toggle);
-  digitalWrite(ledButton, button);
 }
