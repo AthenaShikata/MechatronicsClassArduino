da312d15612982d76f3eecfce44b57a8b6d38c5d
Athena Shikata <99115384+AthenaShikata@users.noreply.github.com>
2022-11-16 10:15:08 -0800

    11-16-22

---
+++ 2022-11-16 LCD Lab 2.ino
@@ -0,0 +1,15 @@
+// include the library code:
+#include <LiquidCrystal.h>
+
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup() {
+  lcd.begin(16, 2);
+  lcd.print("Hello, world!");
+}
+
+void loop() {
+  lcd.setCursor(13, 0);
+  lcd.print(millis() / 1000);
+}
