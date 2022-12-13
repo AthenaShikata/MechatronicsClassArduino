0c13a9b20a17dbcf1af3099801a5c4d96a49903a
Athena Shikata <99115384+AthenaShikata@users.noreply.github.com>
2022-12-13 09:50:02 -0800

    12/13/22

---
+++ 2022-12-6 Rube Goldberg/2022-12-6 Rube Goldberg.ino
@@ -0,0 +1,27 @@
+#include <Servo.h>
+
+const int PR = A1;
+
+Servo launcher;
+
+int val = 0;
+
+void setup() {
+  Serial.begin (9600);
+  launcher.attach (9); 
+  launcher.write (0);
+  pinMode (PR, INPUT);
+}
+
+void loop() {
+  val = analogRead (PR);
+  Serial.println (val);
+  if (val >= 700) {
+    Serial.println ("LET THE CARNAGE COMMENCE");
+    delay(3000);
+    launcher.write (180);
+    delay (1000);
+    launcher.write (0);
+  }
+}
+
