d1b1410d9ce024dd9aceef17d9342fa32effdd88
Athena Shikata <99115384+AthenaShikata@users.noreply.github.com>
2023-01-20 11:03:03 -0800

    Update 2022-11-03 Doll Heads.ino

--- 2022-11-03 Doll Heads/2022-11-03 Doll Heads.ino
+++ 2022-11-03 Doll Heads/2022-11-03 Doll Heads.ino
@@ -6,7 +6,8 @@ Servo shrunk;
 
 const int sensor = 4;
 const int button = 5;
-const int led = 8;
+const int led1 = 8;
+const int led2 = 7;
 
 float angle1 = 80;
 float angle2 = 80;
@@ -25,7 +26,8 @@ void setup() {
   shrunk.attach(9);
   pinMode(sensor, INPUT);
   pinMode(button, INPUT_PULLUP);
-  pinMode(led, OUTPUT);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
   randomSeed(analogRead(0));
 }
 
@@ -39,7 +41,16 @@ void loop() {
   blood.write(angle1);
   burnt.write(angle2);
   shrunk.write(angle3);
+  Serial.print(angle1);
+  Serial.print("    ");
+  Serial.print(angle2);
+  Serial.print("    ");
+  Serial.print(angle3);
+  Serial.print("    ");
   detect = digitalRead(sensor);
+  digitalWrite(led1, detect);
+  Serial.print(detect);
+  Serial.print("    ");
   if (detect == 1) {
     if (state == 0) {
       state = 1;
@@ -47,10 +58,13 @@ void loop() {
       burnt.write(90);
       shrunk.write(90);
       delay(5500);
+      detect = 0;
     }
   } else {
     if (state == 1) {
       state = 0;
     }
   }
+  digitalWrite(led2, state);
+  Serial.println(state);
 }
